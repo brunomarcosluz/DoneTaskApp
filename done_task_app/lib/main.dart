4f0be09a3f44796209ba852b3bc69e2c8f683dde
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-01-15 23:15:41 -0300

    Adicionando novas fontes com o google fonts

--- done_task_app/lib/main.dart
+++ done_task_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const DoneTaskApp());
 
@@ -23,7 +24,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Done. - Do the important.'),
+        title: Text(
+            'Done. - Do the important.',
+             style: GoogleFonts.roboto(),),
         leading: Placeholder(
           color: Colors.red,
         ),
